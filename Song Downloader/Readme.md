e534df0f0b863cfc93d7a7a678166700d4bf7efd
Shubham Kumar <kumarshubham271999@gmail.com>
2018-12-02 03:52:27 +0530

    Added the song downloder script

---
+++ Song Downloader/Readme.md
@@ -0,0 +1,16 @@
+# Requirements
+1. Python v3x
+2. youtube-dl version 2018.09.26
+3. beautifulsoup 4-4.6.3 bs4-0.0.1
+4. emoji script
+
+# How to use
+Simply run the script using-
+<pre>python3 dwdsong.py</pre> 
+Enter the name of the song when asked. It will list you all the choices available.
+It will download the selected list song by default in a folder name MySongs
+
+# Customization
+You can customise the script to change the folder name by initializing the folderName variable with the required folder Name
+
+
---
+++ Song Downloader/dwdsong.py
@@ -0,0 +1,54 @@
+from __future__ import unicode_literals
+import youtube_dl
+import os
+import requests,emoji,sys
+from bs4 import BeautifulSoup as bs
+
+# Add your own folder name
+folderName = 'MySongs'
+
+#Download config:
+opts={
+    'format':'bestaudio/best',
+    'outtmpl':'%(title)s.%(ext)s',
+    'nocheckcertificate':True,
+    'min-views':1000000,
+    'postprocessors':[{
+        'key':'FFmpegExtractAudio',
+        'preferredcodec':'mp3',
+        'preferredquality':'320',
+    }],
+}
+
+def dwdsong(link):
+    if not os.path.exists(folderName):
+        os.mkdir(folderName)
+    else :
+        os.chdir(folderName)
+    print('DOWNLOADING YOUR SONG....')
+    with youtube_dl.YoutubeDL(opts) as dl:
+        dl.download([link])
+    print('SONG DOWNLOADED'+emoji.emojize(':thumbsup:',use_aliases=True))
+
+
+
+
+
+name = str(input("ENTER NAME:"))
+link='https://www.youtube.com/results?search_query='+name.replace(' ','+')+'&sp=CAASAhABQgQIABIA'
+print('GETTING INFORMATION...')
+page = requests.get(link)
+soup = bs(page.text,'html.parser')
+vids = soup.select('.yt-lockup-title')
+print(str(len(vids))+' VIDEOS BY NAME \''+name+'\' FOUND!')
+i=1
+for vid in vids:
+    print(i, vid.text)
+    i=i+1
+
+print("ENTER THE INDEX NUMBER OF THE SONG YOU WANT TO DOWNLOAD")
+index = int(input())
+link = "https://www.youtube.com"+vids[index-1].select('a')[0]['href']
+dwdsong(link)
+
+
