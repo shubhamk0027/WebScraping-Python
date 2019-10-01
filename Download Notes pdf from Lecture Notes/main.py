66322cec22b40a6a9403544364951e71aba4c6ee
nageshwarp201 <nageshwarp201@gmail.com>
2019-10-01 22:50:49 +0530

    Added main code

--- Download Notes pdf from Lecture Notes/main.py
+++ Download Notes pdf from Lecture Notes/main.py
@@ -2,3 +2,16 @@ from fpdf import FPDF
 import requests
 from bs4 import BeautifulSoup as bs
 
+
+base =  "https://lecturenotes.in/"
+page =  open("html.html")   # parsing a saved html
+soup =  bs(page,'html.parser')
+ll   =  soup.select(".pic")
+
+#make image
+def make_image(url,name,x):
+    image = requests.get(url)
+    file = open(name,'wb')
+    file.write(image.content)
+    file.close()
+
